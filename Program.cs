27b932687bdeb0ff10a25f663139be1ded69647a
Aniketjiotode <aniketjiotode07@gmail.com>
2023-01-28 21:11:40 +0530

    Added UC_2 Changes

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace Snake_And_Ladder
             Console.WriteLine("Snake and Ladder game played with single player at start position 0");
             int Player_position = 0;
             Console.WriteLine("Position = " + Player_position);
+            Random random= new Random();
+            int Die_num =random.Next(7);
+            Console.WriteLine($"{Die_num}");
+
            
         }   
     }
