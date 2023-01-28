4d0ed813e625ba088d84c3a0a52f4a501a017462
Aniketjiotode <aniketjiotode07@gmail.com>
2023-01-28 23:37:03 +0530

    Added UC_7 changes

--- Program.cs
+++ Program.cs
@@ -7,39 +7,99 @@ namespace Snake_And_Ladder
         static void Main(string[] args)
         {
             Console.WriteLine("Snake and Ladder game played with single player at start position 0");
-            int Player_position = 0;
-            Console.WriteLine("Position = " + Player_position);
-            Random random= new Random();
-            int Dice_num =random.Next(7);
-            Console.WriteLine($"{Dice_num}");
+            int Player1_position = 0;
+            int Player2_position = 0;
+            Random random = new Random();
+            
 
-            while (Player_position <= 100)
+            while (Player1_position < 100 && Player2_position < 100)
             {
-                int option = random.Next(3);
-                switch (option)
+                bool flag = true;
+                while (flag)
                 {
-                    case 0:
-                        Console.WriteLine("No play");
-                        Console.WriteLine(" position =" + Player_position);
-                        break;
-                    case 1:
-                        Console.WriteLine("Ladder");
-                        Player_position = Player_position + Dice_num;
-                        Console.WriteLine(" position =" + Player_position);
-                        break;
-                    case 2:
-                        Console.WriteLine("Snake");
-                        Player_position = Player_position - Dice_num;
-                        Console.WriteLine(" position =" + Player_position);
-                        break;
+                    int Dice1_num = random.Next(1, 7);
+                    int option1 = random.Next(3);
+                    switch (option1)
+                    {
+                        case 0:
+                            Console.WriteLine("No play");
+                            Console.WriteLine(" PLayer1_position =" + Player1_position);
+                            flag = false;
+                            break;
+                        case 1:
+                            Console.WriteLine("Ladder");
+                            Player1_position = Player1_position + Dice1_num;
+                            Console.WriteLine(" Player1_position =" + Player1_position);
+                            break;
+                        case 2:
+                            Console.WriteLine("Snake");
+                            Player1_position = Player1_position - Dice1_num;
+                            Console.WriteLine(" Player1_position =" + Player1_position);
+                            flag = false;
+                            break;
+                    }
+
+                    if (Player1_position < 0)
+                    {
+                        Player1_position = 0;
+
+                    }
+                    else if (Player1_position > 100)
+                    {
+                        Player1_position = Player1_position - Dice1_num;
+                    }
+                    else if (Player1_position== 100)
+                    {
+                        Console.WriteLine("PLayer1 win the game");
+                        Environment.Exit(0);
+                    }
+                   
                 }
 
-                        if (Player_position < 0)
-                        {
-                            Player_position = 0;
-                            Console.WriteLine(" position = " + Player_position);
-                        }
+                flag = true;
+                while (flag)
+                {
+                    int Dice2_num = random.Next(1, 7);
+                    int option2 = random.Next(3);
+                    switch (option2)
+                    {
+                        case 0:
+                            Console.WriteLine("No play");
+                            Console.WriteLine(" Player2_position =" + Player2_position);
+                            flag = false;
+                            break;
+                        case 1:
+                            Console.WriteLine("Ladder");
+                            Player2_position = Player2_position + Dice2_num;
+                            Console.WriteLine(" Player2_position =" + Player2_position);
+                            break;
+                        case 2:
+                            Console.WriteLine("Snake");
+                            Player2_position = Player2_position - Dice2_num;
+                            Console.WriteLine(" Player2_position =" + Player2_position);
+                            flag = false;
+                            break;
+                    }
+
+                    if (Player2_position < 0)
+                    {
+                        Player2_position = 0;
+
+                    }
+                    else if (Player2_position > 100)
+                    {
+                        Player2_position = Player2_position - Dice2_num;
+                    }else if (Player2_position == 100)
+                    {
+                        Console.WriteLine("player2 win the game");
+                        Environment.Exit(0);
+                    }
+                   
+                }
+                
             }
-        }   
+          
+            
+        }
     }
 }
